31fc15f7a4838aee7232174e77c1321927a749ad
TonyZXY <xyzheng109@gmail.com>
2017-08-16 21:20:29 +1000

    Initial Commit

---
+++ ISE_Assignment1Tests/ISE_Assignment1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ISE_Assignment1Tests.swift
+//  ISE_Assignment1Tests
+//
+//  Created by Xuyang Zheng on 16/8/17.
+//  Copyright © 2017 Xuyang Zheng. All rights reserved.
+//
+
+import XCTest
+@testable import ISE_Assignment1
+
+class ISE_Assignment1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
